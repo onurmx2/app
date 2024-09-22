72a33f352164d84e458fdb8dca1bfbd47aa5b81b
onurmx2 <72471776+onurmx2@users.noreply.github.com>
2024-09-22 15:23:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,34 +4,39 @@ from flask import Flask, redirect
 app = Flask(__name__)
 
 def get_url_from_file():
-    # data.txt dosyasını aç ve ilk satırdaki URL'yi al
+    # data.txt dosyasını aç ve ilk satırdaki kelimeyi al
     with open('data.txt', 'r') as file:
-        url = file.readline().strip()
-    return url
+        query = file.readline().strip()
+    return query
 
 def search_google(query):
-    # googlesearch-python kullanarak ilk arama sonucunu al
+    # Google'da arama yap ve ilk sonucun başlığını al
     search_results = search(query, num_results=1)
     
-    # İlk sonucu döndür
     for result in search_results:
-        return result
+        return result  # İlk sonucu döndür
 
     return None
 
 @app.route('/')
 def home():
-    # data.txt dosyasından URL alalım
-    url_from_file = get_url_from_file()
+    # data.txt dosyasından kelime alalım
+    initial_query = get_url_from_file()
     
-    # Google'da bu URL'yi ara ve ilk sonuca git
-    first_result_url = search_google(url_from_file)
+    # İlk Google araması ve başlıkla beraber URL'yi alalım
+    first_result_url = search_google(initial_query)
     
     if first_result_url:
-        return redirect(first_result_url)  # Kullanıcıyı ilk çıkan sonuca yönlendir
+        # "izle" kelimesiyle tekrar arama yapalım
+        new_query = f"{initial_query} izle"
+        final_result_url = search_google(new_query)
+        
+        if final_result_url:
+            return redirect(final_result_url)  # Kullanıcıyı ilk çıkan sonuca yönlendir
+        else:
+            return "No search results found after 'izle' query."
     else:
-        return "No search results found."
+        return "No search results found for the initial query."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
